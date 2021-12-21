a0bac98eca182a747b1538c40be8e757861b5328
thiennv <thiennv@sotanext.com>
2021-12-22 00:59:16 +0700

    feat: add file

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -2,17 +2,13 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
+use crate::processor::Processor;
+
 entrypoint!(process_instruction);
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
